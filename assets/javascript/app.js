5dbd7a93557a9caea6e62d45e4228517ccf47a4e
Qbisst <ismaelaperez@outlook.com>
2019-03-10 21:13:22 -0400

    more stuff added

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBe9psEr5SwjJ9xIbvjGZWOM1lWk9t5dFk",
+    authDomain: "my-1st-project-d4961.firebaseapp.com",
+    databaseURL: "https://my-1st-project-d4961.firebaseio.com",
+    projectId: "my-1st-project-d4961",
+    storageBucket: "my-1st-project-d4961.appspot.com",
+    messagingSenderId: "353096619593"
+  };
+  firebase.initializeApp(config);
+
+  //Reference to Firebase
+  var database = firebase.database();
+
+  
+
+}) 
